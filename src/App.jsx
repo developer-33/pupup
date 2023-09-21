7601bb9f145346b4e3724895801196209d0aa358
fletcher burton <burton.fletcher@aol.com>
2023-09-21 07:10:01 -0500

    nice

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import AllPlayers from "./components/AllPlayers";
+import NavBar from "./components/NavBar";
+import PlayerDetails from "./components/PlayerDetails";
+
+
+function App() {
+  const [allPlayers, setAllPlayers] = useState([]);
+  useEffect(() => {
+    async function fetchAllPlayers() {
+      try {
+        const response = await fetch(
+          "https://fsa-puppy-bowl.herokuapp.com/api/2306-FTB-ET-WEB-AM/players"
+        );
+        const translatedData = await response.json();
+        setAllPlayers(translatedData.data.players);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchAllPlayers();
+  }, []);
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/players"
+          element={<AllPlayers allPlayers={allPlayers} />}
+        />
+        <Route
+          path="/players/:id"
+          element={<PlayerDetails allPlayers={allPlayers} />}
+        />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
